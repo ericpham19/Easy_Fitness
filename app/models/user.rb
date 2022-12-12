b0d0461d3c875dd79b2c8b1b07370235e65c9c5b
Duy Pham <ericpham19@outlook.com>
2022-12-12 14:09:01 +1100

    updated user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
     has_many :exercises, through: :sessions
 
     validates :username, :email, uniqueness: { case_sensitive: false}
+    validates :username, :email, presence: true
+    validates :email, presence: true, format: { with: /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/, message: "Please try a valid email name" }
+
 end
