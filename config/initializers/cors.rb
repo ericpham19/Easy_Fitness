b5f2f27498b06a0108b7e3db99bf7ee3fcf224a8
Duy Pham <ericpham19@outlook.com>
2022-12-15 21:15:39 +1100

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://63968a4b07648c120f10031a--fastidious-cocada-8350df.netlify.app"
+    origins "https://fastidious-cocada-8350df.netlify.app/"
 
     resource "*",
       headers: :any,
